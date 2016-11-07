cfffce862693cb31c88d323862bed0160711dbd3
Kathryn Middleton <kmiddleton14@gmail.com>
2016-11-07 11:52:30 -0500

    finished command arguments exercise

--- bash.js
+++ bash.js
@@ -8,7 +8,11 @@ process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
+  var input = data.toString().trim(); // remove the newline
+  var arrayOfCmds = input.split(" ");
+  var cmd = arrayOfCmds[0];
+  var args = arrayOfCmds.slice(1);
+
   	if(cmd === 'date'){
   		commands['date']();
   	}
@@ -18,6 +22,17 @@ process.stdin.on('data', function (data) {
     if(cmd === 'ls'){
       commands['ls']();
     }
+    if(cmd === 'echo'){
+      commands['echo'](args);
+    }
+    if(cmd === 'cat'){
+      commands['cat'](args);
+    }
+    if(cmd === 'head'){
+      commands['head'](args);
+    }
+
+
   	process.stdout.write('\nprompt > ');
 
 });
--- commands.js
+++ commands.js
@@ -21,5 +21,50 @@ module.exports = {
 		var dt = new Date();
   		var utcDate = dt.toUTCString();
   		console.log(utcDate);
+	},
+
+	echo: function(argsArray){
+		var output = "";
+		for (var i = 0; i < argsArray.length; i++) {
+			output += argsArray[i] + " "
+		}
+  		return console.log(output);
+	},
+	cat: function(argsArray){
+
+		fs.readFile(argsArray[0], 'utf8', (err, data) => {
+			if(err) throw err;
+			console.log(data);
+
+		});
+	},
+
+	head: function(argsArray){
+
+		var lineReader = require('readline').createInterface({
+			input: fs.createReadStream(argsArray[0])
+
+		});
+
+		var counter = 0;
+
+		lineReader.on('line', function(line){
+			counter++;
+			if(counter < 5) console.log(line);
+		})
+
+		// fs.readFile(argsArray[0], 'utf8', (err, data) => {
+		// 	if(err) throw err;
+			
+		// 	var lines = data.toString().split("\n");
+			
+		// 	for(var i = 0 ; i < 5 ; i++){
+		// 		console.log(lines[i]);
+		// 	}
+
+		// });
+
+
 	}
+
 }
